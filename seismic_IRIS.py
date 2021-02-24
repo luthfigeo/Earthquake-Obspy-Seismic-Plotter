b4a08efa3d0a056ac9e4ab6d2242f7014408630f
Luthfi Saifudin <luthfinurul41@gmail.com>
2021-02-24 14:06:42 +0700

    remove instrument response and calculate ground velocity

--- seismic_IRIS.py
+++ seismic_IRIS.py
@@ -6,15 +6,14 @@ from obspy import UTCDateTime
 
 t = UTCDateTime('2021-01-14T18:28:00')
 [sbef, saft] = [0,2400]
-st = client.get_waveforms('IU','DAV','10','BHZ', t-sbef, t+saft, attach_response = True)
-#st = st.select(component='Z')
-st.plot(linewidth=0.75, equal_scale = False)
+st0 = client.get_waveforms('IU','DAV','10','BHZ', t-sbef, t+saft, attach_response = True)
+st0.remove_response(output='VEL')
+st0.plot(linewidth=0.5, equal_scale = False)
 
+st1 = client.get_waveforms('IU','MBWA','10','BHZ', t-sbef, t+saft, attach_response = True)
+st1.remove_response(output='VEL')
+st1.plot(linewidth=0.5, equal_scale = False)
 
-st = client.get_waveforms('IU','MBWA','10','BHZ', t-sbef, t+saft, attach_response = True)
-#st = st.select(component='Z')
-st.plot(linewidth=0.75, equal_scale = False)
-
-st = client.get_waveforms('IU','PMG','10','BHZ', t-sbef, t+saft, attach_response = True)
-#st = st.select(component='Z')
-st.plot(linewidth=0.75, equal_scale = False)
+st2 = client.get_waveforms('IU','PMG','10','BHZ', t-sbef, t+saft, attach_response = True)
+st2.remove_response(output='VEL')
+st2.plot(linewidth=0.5, equal_scale = False)
